d33b61d97faa1eeb5ed1ef7b5488eb1141dedf76
tkppro <tkpproo@gmail.com>
2019-04-13 21:20:35 +0700

    create model and relationship

---
+++ app/Models/Order.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'payment_id',
+        'total_price',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function payment()
+    {
+        return $this->belongsTo(Payment::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'order_products')
+                    ->withPivot('quantity', 'discount')
+                    ->withTimestamps();
+    }
+}
