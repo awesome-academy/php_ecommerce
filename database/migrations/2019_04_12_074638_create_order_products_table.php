015b71e65a2863e19e31b7cc6bf7ce47dad142e8
tkppro <tkpproo@gmail.com>
2019-04-16 15:24:39 +0700

    restore migration files

---
+++ database/migrations/2019_04_12_074638_create_order_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('order_id')->index();
+            $table->integer('product_id')->index();
+            $table->integer('quantity');
+            $table->integer('discount')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_products');
+    }
+}
